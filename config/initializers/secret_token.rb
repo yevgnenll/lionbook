52e7397f7eaca5590f0ac823dc6f506f81ead9dd
root <root@ip-172-31-9-39.ap-northeast-1.compute.internal>
2015-07-07 13:47:17 +0000

    first commit ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lionbook::Application.config.secret_key_base = '915dbea5c0457def797f3b229293e43ce836a6aea0993b5232078175dd80629d3b8ff09a9e29572b169a53dd8cc0662c02e06af9131cc28478c3ecffdcf3fbc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lionbook::Application.config.session_store :cookie_store, key: '_lionbook_session'
